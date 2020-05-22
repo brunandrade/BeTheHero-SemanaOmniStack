31415623e64f72eee171f3e689a921b803fd4781
brunandrade <bruna.card.andrade@gmail.com>
2020-05-22 18:54:26 -0300

    Finalização da conexão do banckend com frontend

--- frontend/src/pages/Logon/Index.js
+++ frontend/src/pages/Logon/Index.js
@@ -1,20 +1,48 @@
-import React from 'react';
-import{Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import{Link, useHistory} from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
+import api from '../../services/api'
+
 import './styles.css'
 import heroesImg from '../../assests/heros.png';
 import logoImg from '../../assests/logo.svg';
 
+
 export default function Logon(){
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+        //const data = { id };
+        try {
+            const response = await api.post('sessions', {id})
+            //console.log(response.data.name);
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+
+        } 
+        catch (err) {
+            alert('Falha no Login, tente novamente');
+        }
+    }
+
     return(
      <div className="logon-container">
          <section className="form">
 
             <img src={logoImg} alt="Be The Hero"/>
 
-            <form>
+            <form onSubmit={handleLogin}>
                 <h1> Faça seu Logon</h1>
-                <input placeholder="Sua ID"/>
+                <input 
+                placeholder="Sua ID"
+                value={id}
+                onChange={e => setId(e.target.value)}
+                />
                 
                 <button className="button"type="submit"> Entrar</button>
                 
@@ -28,9 +56,5 @@ export default function Logon(){
 
          <img src={heroesImg} alt="Heroes"/>
      </div>
-
-
-
     );
-
 }
